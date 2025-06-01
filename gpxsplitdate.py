a37d86b703521dbc094f8893aa104fd19f746666
Artem Svetlov <trolleway@yandex.ru>
2025-06-02 00:35:48 +0300

    timedelta direction

--- gpxsplitdate.py
+++ gpxsplitdate.py
@@ -24,22 +24,29 @@ def parse_timedelta(time_str):
     hours, minutes = map(int, time_str[1:].split(':'))
     return timedelta(hours=hours * sign, minutes=minutes * sign)
     
-def split_gpx_by_datesfilename(filename:str,resultdir:str,unique_dates:list, timezone_text:str=''):
+def split_gpx_by_dates(filename:str,resultdir:str,unique_dates:list, timezone_text:str=''):
+
+    from pathlib import Path
+    Path(resultdir).mkdir(parents=True, exist_ok=True)
 
     for date_string in sorted(unique_dates):
         date_object = datetime.strptime(date_string, "%Y-%m-%d")  # Convert string to datetime object
         
-        date_object = date_object + parse_timedelta(timezone_text)
+        date_object = date_object - parse_timedelta(timezone_text)
         
         start_date_string = date_object.strftime("%Y%m%d%H%M%S")
         new_date = date_object + timedelta(days=1)  # Add one day
         end_date_string = new_date.strftime("%Y%m%d%H%M%S")  # Convert back to string
-        cmd = f'''gpsbabel -i gpx -f {filename} -o gpx  -x "track,start={start_date_string},stop={end_date_string}"  -F {resultdir}/{date_string[:10]}.gpx '''
+        cmd = f'''gpsbabel -i gpx -f {filename} -o gpx  -x "track,start={start_date_string},stop={end_date_string},title="%Y%m%d""  -F {resultdir}/{date_string[:10]}.gpx '''
         
         print(cmd)
         os.system(cmd)
         
         #gpsbabel -i gpx -f merge.gpx -o gpx -x track,start=202505090000,stop=20250510000000 -F 2025-05-09.gpx
+        
+        '''
+        gpsbabel -t -i gpx -f  example/merge.gpx -x track,pack,split,title="ACTIVE LOG # %Y%m%d" -o gpx -F "dev_%Y%m%d.gpx"
+        '''
 
 def parse_arguments():
     parser = argparse.ArgumentParser(description="Split GPX tracks by day and store in a directory.")
@@ -50,12 +57,6 @@ def parse_arguments():
         type=str,
         default="0:00:00",
         help="Optional timezone offset in the format 'H:M:S', representing a Python timedelta string."
-    )
-    parser.add_argument(
-        "--output_dir",
-        type=str,
-        default="output",
-        help="Directory where the split GPX tracks will be stored."
     )
 
     return parser.parse_args()
@@ -76,7 +77,7 @@ if __name__ == "__main__":
     for date in sorted(unique_dates):
         print(date)
         
-    split_gpx_by_dates(filename=filename,resultdir=resultdir,unique_dates=unique_gates, timezone_text=timezone_text)
+    split_gpx_by_dates(filename=filename,resultdir=resultdir,unique_dates=unique_dates, timezone_text=timezone_text)
     
     
 
